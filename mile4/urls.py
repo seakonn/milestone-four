5cd277f7a25f75f9cd87582feae9546e82aa4fd5
seakonn <sean.ocon@gmail.com>
2020-05-18 15:15:59 +0000

    Removed references to old project

--- mile4/urls.py
+++ mile4/urls.py
@@ -1,4 +1,4 @@
-"""django_auth URL Configuration
+"""mile4 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
