5449c95d56c3435fdf0eb9385fff0901add85c9f
seakonn <sean.ocon@gmail.com>
2020-05-20 12:37:37 +0000

    Added basic model and form for commission

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Commission(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    #type = models.CharField(label='TEST LABEL', widget=models.Select(choices=COMMISSION_TYPES))
+
+    #def __str__(self):
+     #   return self.name
