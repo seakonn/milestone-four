3d61d241b3f497f13d159ed53a0d0e7ae087892f
seakonn <sean.ocon@gmail.com>
2020-05-23 09:55:03 +0000

    Added basic dropdown menu

--- commissions/forms.py
+++ commissions/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from .models import Commission
 
+TYPES = [
+    ('statue', 'Statue'),
+    ('large_portrait', 'Large Portrait')
+]
+
 
 class CommissionForm(forms.ModelForm):
 
     class Meta:
         model = Commission
         fields = ('name', 'description')
+
+    type = forms.CharField(label='Commission Type', widget=forms.Select(choices=TYPES))
