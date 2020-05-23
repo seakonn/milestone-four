a34897c01e5844edc7f2f54ee24ef9f298beee4c
seakonn <sean.ocon@gmail.com>
2020-05-23 12:02:27 +0000

    Dropdown box now working

--- commissions/forms.py
+++ commissions/forms.py
@@ -12,5 +12,3 @@ class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission
         fields = ('name', 'description')
-
-    type = forms.CharField(label='Commission Type', widget=forms.Select(choices=TYPES))
