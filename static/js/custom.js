0a34a094aa352b4c86d4f48d5937d3da02a72cba
seakonn <sean.ocon@gmail.com>
2020-06-07 10:37:07 +0000

    Added comment to js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,22 @@
-document.addEventListener("load", displayMessageBox())
+document.addEventListener("load", displayMessageBox());
 
+/*
+Function checks to see if a message exists after the page loads.
+If it does, it hides the associated div element and displays
+the message in an alert box. If there is no message, the 
+function doesn't do anything.
+*/
 function displayMessageBox() {
     
+    let message;
+    try {
+        message = document.getElementById("message-popup").innerHTML
+    }
+
+    catch(error) {
+        console.log("Error is " +error)
+        message = null;
+    }
+    
+
 }
