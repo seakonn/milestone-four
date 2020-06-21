d853050f1cb900bcbb30909b533baf03f90ddaec
seakonn <sean.ocon@gmail.com>
2020-06-21 11:25:00 +0000

    Added settings for amazon s3 bucket storage

--- mile4/settings.py
+++ mile4/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'commissions',
-    'checkout'
+    'checkout',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,22 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+
+AWS_STORAGE_BUCKET_NAME = 'ozzys-art'
+AWS_S3_REGION_NAME = 'eu-west-1'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
